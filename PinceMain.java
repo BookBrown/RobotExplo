568afcf56839e4599d3b0f166ecac0dab1ebbc17
BookBrown <94379465+BookBrown@users.noreply.github.com>
2022-03-09 13:57:36 +0100

    Update PinceMain.java

--- PinceMain.java
+++ PinceMain.java
@@ -14,25 +14,28 @@ import lejos.utility.Delay;
 public class PinceMain {
 	public static void main(String[] args) {
 		
-		Pince pince = new Pince();
+		//Pince pince = new Pince();
 		
-		Delay.msDelay(2000);
+		//Delay.msDelay(2000);
 		
-		pince.premiere_recup();
+		//pince.premiere_recup();
 		
-		pince.deuxieme_recup();
+		//pince.deuxieme_recup();
 		
-		pince.largage();
+		//pince.largage();
 		
 		// get a port instance
-		//Port port = LocalEV3.get().getPort("S2");
+		Port port = LocalEV3.get().getPort("S2");
 
 		// Get an instance of the Ultrasonic EV3 sensor
-		//SensorModes sensor = new EV3UltrasonicSensor(port);
+		SensorModes sensor = new EV3UltrasonicSensor(port);
 		
-		//float distance = localisation.detecte(sensor);
+		float distance = localisation.detecte(sensor);
 		
-		//System.out.println(distance);
+		System.out.println(distance);
+		
+		LCD.clear(2);
+		LCD.drawString(" distance: "+distance, 0, 4);
 
 	}
 }
