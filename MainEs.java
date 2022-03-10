db36ad801f2c05850d07b71b18bc9056726efbb3
BookBrown <94379465+BookBrown@users.noreply.github.com>
2022-03-10 11:19:59 +0100

    Add files via upload

---
+++ MainEs.java
@@ -0,0 +1,51 @@
+package robot;
+
+import lejos.hardware.ev3.LocalEV3;
+import lejos.hardware.port.Port;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
+import lejos.robotics.SampleProvider;
+import lejos.utility.Delay;
+
+
+public class MainEs {
+	public static void main(String[] args) {
+		
+		//Pince pince = new Pince();
+		
+		//Delay.msDelay(2000);
+		
+		//pince.premiere_recup();
+		
+		//pince.deuxieme_recup();
+		
+		//pince.largage();
+		
+		// get a port instance
+		Port port = LocalEV3.get().getPort("S4");
+
+		// Get an instance of the Ultrasonic EV3 sensor
+		EV3UltrasonicSensor sensor = new EV3UltrasonicSensor(port);
+		
+		// get an instance of this sensor in measurement mode
+		SampleProvider distanceSample= sensor.getDistanceMode();
+		
+		float distance = localisation.detecte(distanceSample);
+		
+		System.out.println(distance);
+		
+		Delay.msDelay(4000);
+		
+		//initialise the motors
+		Mouvement motors = new Mouvement();
+		
+		//on fait maintenant la phase d'approche (on suppose avoir repéré le robot).
+		
+		
+		
+		approche.approcheTarget(distanceSample, motors);
+		
+		
+		sensor.close();
+
+	}
+}
