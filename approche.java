af17b093eab6c7433c84d06b7523dcc1326bbeea
BookBrown <94379465+BookBrown@users.noreply.github.com>
2022-03-10 19:13:49 +0100

    Update approche.java

--- approche.java
+++ approche.java
@@ -14,6 +14,7 @@ public class approche {
 		int angleRelatifMinimum;
 		int compteurGauche;
 		int compteurDroit;
+		//boolean isRecuperationZone=false;
 		
 		while((distance >30)&&(distance<50)){
 			
@@ -37,7 +38,7 @@ public class approche {
 			
 			if(newdistance==-100){
 				
-				motors.tourner(100, 60); //on se met 15° à droite de la position initiale
+				motors.tourner(100, 60); //on se met 15degrés a droite de la position initiale
 				angleRelatifMinimum= 15;
 				
 				newdistance = 100*localisation.detecte(sample);
@@ -69,6 +70,23 @@ public class approche {
 			
 			distance = newdistance;
 		}
+		
+		//On teste ci-dessous si on est face a la zone de récupération ou un capteur.
+		/*boolean seenLeft= false;
+		boolean seenRight= false;
+		motors.tourner(30,-25);
+		newdistance= 100*localisation.detecte(sample);
+		if((newdistance <70)&&(newdistance>0)){
+			seenLeft = true;
+		}
+		motors.tourner(30,50);
+		newdistance= 100*localisation.detecte(sample);
+		if((newdistance <70)&&(newdistance>0)){
+			seenRight = true;
+		}
+		
+		isRecuperationZone = seenRight&&seenLeft; //on a bien que si on détecte à 25 degres a gauche et a droite, on est face a la zone de recuperation
+		*/
 			
 		while((distance > 20)&&(distance<30)){
 			
@@ -90,7 +108,7 @@ public class approche {
 			
 			if(newdistance==-100){
 				
-				motors.tourner(10, 4); //on se met 15° à droite de la position initiale
+				motors.tourner(10, 4); //on se met 15degres a droite de la position initiale
 				angleRelatifMinimum= 1;
 				
 				newdistance = 100*localisation.detecte(sample);
@@ -122,6 +140,8 @@ public class approche {
 			
 			distance = newdistance;
 		}
+		
+		//return isRecuperationZone;
 	}
 		
 }
