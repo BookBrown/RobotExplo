e5b12a1a361317a2b0d7ba4cb0a8b5e1acf5a21d
Judi78 <101000220+Judi78@users.noreply.github.com>
2022-03-11 15:36:15 +0100

    Update Borders.java
    Borders sans la localisation mais qui fonctionne

--- Borders.java
+++ Borders.java
@@ -1,12 +1,14 @@
 package robot;
 
-import java.time.Duration;
-import java.time.Instant;
+//import java.time.Duration;
+//import java.time.Instant;
 
 import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.EV3ColorSensor;
 import lejos.hardware.sensor.EV3UltrasonicSensor;
+import lejos.hardware.sensor.SensorMode;
 import lejos.robotics.SampleProvider;
+import lejos.utility.Delay;
 
 public class Borders {
 	
@@ -20,99 +22,117 @@ public class Borders {
 		this.echantillon = false;
 	}
 	
-	public float Init(EV3ColorSensor Csensor, SampleProvider USsensor){
+	public float Init(SampleProvider csensor){
+		
+		float[] coul = new float[1];
+		//, SampleProvider USsensor 
 		
 		Mouvement mouv = new Mouvement();
 		
         float distance = -1;
 
-		if (Csensor.getColorID() != black){
+        csensor.fetchSample(coul,0);
+        
+		if (coul[0] != black){
 			System.out.println("le robot n'est pas dans la zone de depart");
+			Delay.msDelay(5000);
+			distance = -2;
 		}
 		else{
-			mouv.avancer(10,20);
-			distance = localisation.detecte(USsensor);
-			if (distance != -1){
-				echantillon = true;
-				return distance;
-				}
+			mouv.avancer(10,40);
+			//distance = localisation.detecte(USsensor);
+			//if (distance != -1){
+				//echantillon = true;
+				//return distance;
+				//}
 			mouv.tourner(10,90);
-			distance = localisation.detecte(USsensor);
-			if (distance != -1){
-				echantillon = true;
-				return distance;
-				}
+			//distance = localisation.detecte(USsensor);
+			//if (distance != -1){
+				//echantillon = true;
+				//return distance;
+				//}
 			mouv.tourner(10, 180);
-			distance = localisation.detecte(USsensor);
-			if (distance != -1){
-				echantillon = true;
-				return distance;
-				}
+			//distance = localisation.detecte(USsensor);
+			//if (distance != -1){
+				//echantillon = true;
+				//return distance;
+				//}
 		}
         return distance;
 	}
 	
 	
-	public float parcours(EV3ColorSensor Csensor, SampleProvider USsensor){
+	public float parcours(SampleProvider Csensor){
+		
+		//SampleProvider USsensor
 		
 		Mouvement mouv = new Mouvement();
 		
         float distance = -1;
 		boolean droite = true;
 		
-		Instant start = Instant.now();
-		Instant finish = Instant.now();
-		long timelapse = Duration.between(start,finish).toMillis();
+		//Instant start = Instant.now();
+		//Instant finish = Instant.now();
+		//long timelapse = Duration.between(start,finish).toMillis();
 		
-		while (! echantillon && timelapse < 30000){
+		float[] coul = new float[1];
+		int t = 0;
+		
+		while (! echantillon && (t<3)){
+			t++;
+			//timelapse < 30000
 			
-			int d = 0;
-			
-			while (Csensor.getColorID() == white){
+			//int d = 0;
+			Csensor.fetchSample(coul,0);
+			while (coul[0] == white){
 				mouv.avancer(10,5);
-				d++;
-				if(d == 10){
-					distance = localisation.detecte(USsensor);
-					if (distance != -1){
-						echantillon = true;
-						return distance;
-						}
-					d=0;
-				}
+				Csensor.fetchSample(coul,0);
+				//d++;
+				//if(d == 10){
+					//distance = localisation.detecte(USsensor);
+					//if (distance != -1){
+						//echantillon = true;
+						//return distance;
+						//}
+					//d=0;
+				//}
 			}
 			
 			int compteur = 0;
 			
 			if (droite == true){		
-				mouv.avancer(10,-5);
+				mouv.avancer(10,-20);
 				mouv.tourner(10,90);
-				while ((Csensor.getColorID() == white) && (compteur < 6)){
+				Csensor.fetchSample(coul,0);
+				while ((coul[0] == white) && (compteur < 6)){
 					mouv.avancer(10,5);
 					compteur++;
+					Csensor.fetchSample(coul,0);
 				}
 				mouv.tourner(10,90);
-				distance = localisation.detecte(USsensor);
+				//distance = localisation.detecte(USsensor);
 				droite = false;
 			}
 			else {
-				mouv.avancer(10,-5);
+				mouv.avancer(10,-20);
 				mouv.tourner(10,-90);
-				while ((Csensor.getColorID() == white) && (compteur < 6)){
+				while ((coul[0] == white) && (compteur < 6)){
 					mouv.avancer(10,5);
 					compteur++;
+					Csensor.fetchSample(coul,0);
 				}
 				mouv.tourner(10,-90);
-				distance = localisation.detecte(USsensor);
+				//distance = localisation.detecte(USsensor);
 				droite = true;
 			}
 			
-			if (distance != -1){
-				echantillon = true;
-				return distance;
-				}
+			//if (distance != -1){
+				//echantillon = true;
+				//return distance;
+				//}
 			
-			finish = Instant.now();
-			timelapse = Duration.between(start,finish).toMillis();
+			//finish = Instant.now();
+			//timelapse = Duration.between(start,finish).toMillis();
 		}
         return distance;
 	}
