66250db3975fed63259caf8edccda834573524e0
Judi78 <101000220+Judi78@users.noreply.github.com>
2022-03-11 16:08:42 +0100

    Update BordersMain.java

--- BordersMain.java
+++ BordersMain.java
@@ -6,21 +6,28 @@ import java.time.Instant;
 import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.EV3ColorSensor;
 import lejos.hardware.sensor.EV3UltrasonicSensor;
+import lejos.hardware.sensor.SensorMode;
 import lejos.robotics.SampleProvider;
 
 public class BordersMain {
 	public static void main(String[] args) {
 		
-		EV3ColorSensor Csensor = new EV3ColorSensor(SensorPort.S3);
-		EV3UltrasonicSensor sensor = new EV3UltrasonicSensor(SensorPort.S4);
+		EV3ColorSensor Colorsensor = new EV3ColorSensor(SensorPort.S1);
+		SampleProvider Csensor = Colorsensor.getColorIDMode();
 		
+		EV3UltrasonicSensor sensor = new EV3UltrasonicSensor(SensorPort.S3);
 		SampleProvider USsensor= sensor.getDistanceMode();
 		
 		Borders bord = new Borders();
-		float distance = bord.Init(Csensor, sensor);
-		distance = bord.parcours(Csensor, sensor);
+		float distance = bord.Init(Csensor, USsensor);
+		if (distance != -2){
+			distance = bord.parcours(Csensor, USsensor);
+		}
 		
-		System.out.println("Un objet se trouve à :" + distance);
+		System.out.println("Un objet se trouve Ã  :" + distance);
+		
+		sensor.close();
+		Colorsensor.close();
 		
 		}
 	}
